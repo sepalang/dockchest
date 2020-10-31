2f5fa71e7a3a68b9e0bff7551c93c78a6043a5d3
AHN HO JUNG <labeldock@me.com>
2020-10-31 17:42:29 +0900

    ready to publish

--- mysql/docker-entrypoint-initdb.d/user.sh
+++
@@ -1,8 +0,0 @@
-
-mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "create user 'foo'@'%' identified by 'foo';"
-mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "grant all privileges on *.* to 'foo'@'%';"
-echo "Added user [ foo ]"
-mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "create user 'bar'@'%' identified by 'bar';"
-mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "grant all privileges on *.* to 'bar'@'%';"
-echo "Added user [ bar ]"
-mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "flush privileges;"
